c6f2fddb1c9c13f478fb50c31ffc561d7518c347
AndrewBartholomew62 <100846571+AndrewBartholomew62@users.noreply.github.com>
2022-03-14 14:30:32 +0000

    Add files via upload

--- include/draw.h
+++ include/draw.h
@@ -40,6 +40,14 @@ class metapost_control
 
 public:
 
+	enum smoothed 
+	{
+		NON_SEIFERT_SMOOTHED = -1,
+		SEIFERT_SMOOTHED = 1,
+		A_SMOOTHED = 2,
+		B_SMOOTHED = 3
+	};
+
 
 	double frame_minx;
 	double frame_maxx;
@@ -51,15 +59,16 @@ public:
 	double rotation_degrees;
 	double rotation_centre_x;
 	double rotation_centre_y;
-	int rotation_centre_z;	
-	int unit_size;
-	int disc_size;
-	int smoothed_state_disc_size;
-	int pen_size;
-	int horizontal_units;
-	int vertical_units;
-	int infinite_cycle;
 	int arrowhead_bp_size;	
+	int disc_size;
+	int grid_size;
+	int horizontal_units;
+	int infinite_cycle;
+	int pen_size;
+	int rotation_centre_z;	
+	int smoothed_state_disc_size;
+	int unit_size;
+	int vertical_units;
 	float cudgel_space;
 	bool dash_with_dots;
 	bool knotoid_leg_unbounded;
@@ -69,13 +78,14 @@ public:
 	bool draw_immersion;
 	bool draw_crossing_features;
 	bool draw_frame_corners;
-	bool draw_immersion_bounds;
+	bool draw_grid;
 	bool draw_triangulation;
 	bool draw_triangulation_displacement;
 	bool draw_labels;
 	bool label_edges_from_one;
 	bool draw_oriented;
 	bool draw_shortcut;
+	bool gauss_labels;
 	bool label_vertices;
 	bool script_labels;
 	bool scriptscript_labels;
@@ -89,25 +99,28 @@ public:
 	bool adjacent_cudgel_midpoints;
 	bool right_originating_tail_points;
 	bool left_terminating_tail_points;
+	bool magnify_small_circles;
 	bool midpoints_not_tail_points;
 	float midpoint_tension;
 	string colourmap;
 	string singlecolour;
+	string state;
 	vector<string> draw_colour;
 	vector<pair<int,int> > lace_midpoints;
 	vector<tuple<int,int,int> > translations;
 	
 	metapost_control(): frame_minx(0), frame_maxx(0), frame_miny(0),frame_maxy(0),rotate(false), explicit_rotation_centre(false), implicit_rotation_centre(false),
-	                    rotation_degrees(0), rotation_centre_x(0) , rotation_centre_y(0), rotation_centre_z(0),
-	                    unit_size(20), disc_size(30), smoothed_state_disc_size(6), pen_size(1), horizontal_units(5), vertical_units(5),
-						infinite_cycle(-1), arrowhead_bp_size(6),cudgel_space(1.0), dash_with_dots(false), knotoid_leg_unbounded(false), one_metapost_path(false),
-						colour(false), draw_lace_frame(false), draw_immersion(true), draw_crossing_features(true), draw_frame_corners(false), draw_immersion_bounds(false),
+	                    rotation_degrees(0), rotation_centre_x(0) , rotation_centre_y(0), 
+	                    arrowhead_bp_size(6),disc_size(30),grid_size(10), horizontal_units(5), infinite_cycle(-1),
+	                    pen_size(1),rotation_centre_z(0),smoothed_state_disc_size(6), unit_size(20), vertical_units(5),
+						cudgel_space(1.0), dash_with_dots(false), knotoid_leg_unbounded(false), one_metapost_path(false),
+						colour(false), draw_lace_frame(false), draw_immersion(true), draw_crossing_features(true), draw_frame_corners(false), draw_grid(false),
 						draw_triangulation(false), draw_triangulation_displacement(true), draw_labels(false), label_edges_from_one(false), draw_oriented(false), 
-						draw_shortcut(false), label_vertices(false), script_labels(false), scriptscript_labels(false), show_odd_parity_crossings(false), 
+						draw_shortcut(false), gauss_labels(false), label_vertices(false), script_labels(false), scriptscript_labels(false), show_odd_parity_crossings(false), 
 						show_vertex_axes(true), smallarrowheads(false),circle_packing(false), draw_shrink_effect(false),
 	                    highlight_small_edges(false),tension(false),adjacent_cudgel_midpoints(true), right_originating_tail_points(true),
-						left_terminating_tail_points(false), midpoints_not_tail_points(false),midpoint_tension(1.0),colourmap(""),singlecolour(""),
-	                    draw_colour({"red","blue","ForestGreen","Brown","DarkViolet","Orange"}){}	
+						left_terminating_tail_points(false), magnify_small_circles(false),midpoints_not_tail_points(false),midpoint_tension(1.0),colourmap(""),singlecolour(""),
+	                    state(""),draw_colour({"red","blue","ForestGreen","Brown","DarkViolet","Orange"}){}	
 
 };
 
