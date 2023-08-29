74956ca156e04b8bcdc949ef15f2dcdf50f3d17e
AndrewBartholomew62 <100846571+AndrewBartholomew62@users.noreply.github.com>
2023-08-29 15:09:06 +0100

    Delete include directory

--- include/debug.h
+++
@@ -1,8 +0,0 @@
-/**************************************************
-debug.h describes via pre-processor directives, which
-debug capabilities need to be supported by the debug
-subsystem	
-**************************************************/
-
-//#define DEBUG_BIGINT
-
--- include/initialize.h
+++
@@ -1,7 +0,0 @@
-/**************************************************
-initialize.h describes via pre-processor directives, which classes need to be supported by 
-class-control and the debug subsystem	
-**************************************************/
-
-#define INITIALIZE_MATRIX
-
